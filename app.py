cc98ee672f0e2ace476fe8392a29734913d961e7
Dodo <s224965@studenti.polito.it>
2018-04-27 20:14:29 +0100

    I think it is finally done!

--- app.py
+++ app.py
@@ -1,12 +1,42 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template, request
+import db
+from db import checkPresente
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return redirect( url_for("arrival") )
+
+@app.route("/index")
+def arrival():
+    listato = db.showTasks()
+    IDs = db.parallelIDs()
+    num = len(listato)
+    indici = []
+    i=0
+    while i<num:
+        indici.append(i)
+        i=i+1
+    return render_template("lab5.html", N = num, lista = listato, indici = indici, IDs = IDs)
+
+@app.route("/insertNewTask", methods=["POST"])
+def insert():
+    nuovo = request.form["txtbox"]
+    presente = db.checkPresente(nuovo)
+    if presente==-1:
+        db.newTask(nuovo)
+        return redirect(url_for("arrival"))
+    return render_template("newTaskError.html", task=nuovo, codice=0)
+
+@app.route("/deleteTask/<task_id>")
+def delete(task_id):
+    print(task_id)
+    db.removeTaskByID(task_id)
+    return redirect(url_for("arrival"))
 
 
 if __name__ == '__main__':
+    db.init()
     app.run()
--- db.py
+++ db.py
@@ -1,7 +1,8 @@
 import pymysql
-from sys import argv
 
 tasks_list = []
+ID_list = []
+
 
 def init():
     # initialize the task list
@@ -9,16 +10,19 @@ def init():
         # open the file
         #txt = open(filename)
 
+        global tasks_list
+        global ID_list
+
         #SQL
         connection = pymysql.connect(user="root",password="dodo",database="lab5",host="localhost")
-        sql = """
-        SELECT todo
-        FROM task
-        WHERE done=FALSE;
-        """
+        sql = "SELECT todo,id FROM task WHERE done=0;"
+        #sql = "SELECT * FROM task;"
         cursor = connection.cursor()
         cursor.execute(sql)
-        tasks_list = cursor.fetchall()
+        tuple = cursor.fetchall()
+        for tupla in tuple:
+            tasks_list.append(tupla[0])
+            ID_list.append(tupla[1])
         cursor.close()
         connection.close()
         #fine sql
@@ -27,24 +31,26 @@ def init():
         print("File not found!")
     return
 
-def checkPresente(connection,testo):
-    sql = """
-        SELECT id
-        FROM task
-        WHERE (todo=%s)
-        """
+def checkPresente(testo):
+    connection = pymysql.connect(user="root", password="dodo", database="lab5", host="localhost")
     cursor = connection.cursor()
+    sql = """SELECT id
+        FROM task
+        WHERE (todo=%s) AND (done=0);"""
+    #cursor=connection.cursor()
     cursor.execute(sql, (testo,))
-    trovato = cursor.fetchone()
-    if trovato > 0:
-        return trovato
+    primo = cursor.fetchone()
+    cursor.close()
+    connection.close()
+    if primo != None:
+        return primo[0]
     return -1
 
 
 def newTask(testo):
     connection = pymysql.connect(user="root",password="dodo",database="lab5",host="localhost")
     cursor = connection.cursor()
-    trovato = checkPresente(connection,testo)
+    trovato = checkPresente(testo)
     if trovato == -1:
         sql = """
         INSERT INTO task(todo,done)
@@ -52,16 +58,34 @@ def newTask(testo):
         """
         cursor.execute(sql, (testo,) )
         tasks_list.append(testo)
+        sql = """SELECT id
+        FROM task
+        WHERE done=0 AND todo=%s"""
+        cursor.execute(sql, (testo,))
+        tupla = cursor.fetchone()
+        ID_list.append(tupla[0])
+        print(tupla[0])
+        connection.commit()
+        ritorno=0
+    else:
+        ritorno=-1
     cursor.close()
     connection.close()
-    return #fai qualcosa se non trova
+    return ritorno
 
 def showTasks():
     return tasks_list
 
+def parallelIDs():
+    return ID_list
+
 def removeTask(testo):
+
+    global tasks_list
+    global ID_list
+
     connection = pymysql.connect(user="root",password="dodo",database="lab5",host="localhost")
-    presente = checkPresente(connection,testo)
+    presente = checkPresente(testo)
     cursor = connection.cursor()
     if presente!=-1:
         sql = """
@@ -70,9 +94,61 @@ def removeTask(testo):
         WHERE todo = %s
         """
         cursor.execute(sql, (testo,) )
+        connection.commit()
+        ritorno = 0
+    else:
+        ritorno = -1
+    cursor.close()
+    connection.close()
+
+    tr=0
+    i=0
+    N = len(tasks_list)
+    while tr==0 and i<N:
+        if tasks_list[i]==testo:
+            tr=1
+        else:
+            i=i+1
+
+    del(ID_list[i])
+    del(tasks_list[i])
+    #tasks_list.remove(testo)
     #else:
     #errore
+    return ritorno
+
+def removeTaskByID(ID):
+    connection = pymysql.connect(user="root", password="dodo", database="lab5", host="localhost")
+    cur = connection.cursor()
+    sql = """UPDATE task
+    SET done = 1
+    WHERE id=%s
+    """
+    cur.execute(sql, (ID,) )
+    connection.commit()
+    cur.close()
+    connection.close()
+
+    tr=0
+    i=0
+    N = len(tasks_list)
+    while i<N and tr==0:
+        if ID_list[i]==int(ID):
+            tr=1
+        else:
+            i=i+1
+    print(i)
+    del(ID_list[i])
+    del(tasks_list[i])
+    return
 
 if __name__ == '__main__':
     # main program
-    pass
+    #newTask("suca scemo")
+    init()
+    print(tasks_list)
+    print(ID_list)
+    #removeTaskByID(15)
+    #print(tasks_list)
+    #removeTaskByID(11)
+    #print(tasks_list)
